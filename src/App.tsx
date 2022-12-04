7fdaeb51e89ef3e40384dfe4eb0e75b16356f2d8
liusp <thodsaphonsp@gmail.com>
2022-12-04 07:40:23 +0700

    finish modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
-import './App.css';
+
+import {Modal} from "./Modal";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [isOpen, setIsOpen] = useState(false);
+    return (
+        <main>
+            <button className={"primaryBtn"} onClick={() => setIsOpen(true)}>
+                Open Modal
+            </button>
+            {isOpen && <Modal setIsOpen={setIsOpen} />}
+        </main>
+    );
 }
 
 export default App;
